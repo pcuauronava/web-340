e3322327d4e5d5a5634e3ba1fb936405ccce73be
Patrick Cuauro <pcuauronava@my365.bellevue.edu>
2023-01-13 17:52:17 -0500

    tasks completed
    folder created, package.json created, pino installed required version and moment installed required version

---
+++ week-2/sports-app/index.js
@@ -0,0 +1,10 @@
+/*
+===========================================
+; Title: index.js
+; Author: Professor Krasso
+; Date: 01 13 2023
+; Modified by: Patrick Cuauro
+; Description: NPM dependencies work.
+===========================================
+*/
+console.log("Hello, world!");
